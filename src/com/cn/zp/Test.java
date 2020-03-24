5d8e07a70af127457920eb25e4437fbc4a61be27
zhangpeng <zhangpeng@dongfangfuli.com>
2020-03-24 15:55:28 +0800

    可爱的小kula

---
+++ src/com/cn/zp/Test.java
@@ -0,0 +1,11 @@
+package com.cn.zp;
+
+/**
+ * @Author kula
+ * @create 2020/3/24 下午3:50
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("可爱的小Kula");
+    }
+}
