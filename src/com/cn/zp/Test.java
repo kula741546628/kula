844d72ee95ca01136c3650b3b1182e6ddd097526
zhangpeng <zhangpeng@dongfangfuli.com>
2021-06-05 17:26:57 +0800

    test6.5

--- src/com/cn/zp/Test.java
+++ src/com/cn/zp/Test.java
@@ -7,7 +7,7 @@ package com.cn.zp;
 public class Test {
     public static void main(String[] args) {
         System.out.println("可爱的小Kula");
-
+        System.out.println("test");
         System.out.println();
     }
 }
